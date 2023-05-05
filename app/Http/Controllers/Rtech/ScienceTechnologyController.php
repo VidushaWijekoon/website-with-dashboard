b9d2052c2dfcf16442a0cf9da70d9767e2f21a9b
unknown <vidusha.wijekoon11@gmail.com>
2023-05-05 16:50:51 +0400

    basic science and technology page

--- app/Http/Controllers/Rtech/ScienceTechnologyController.php
+++ app/Http/Controllers/Rtech/ScienceTechnologyController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Rtech;
 
-use App\Http\Controllers\Controller;
+use App\Models\Country;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class ScienceTechnologyController extends Controller
 {
